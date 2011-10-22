5cc878b5a8de2bd15b9a26a009557c03a7a0022d
Roger Braun <davinelulinvega@gmail.com>
2011-10-23 00:50:29 +0200

    more tests

--- tests/showdown_spec.js
+++ tests/showdown_spec.js
@@ -3,7 +3,7 @@ var showdown = require("../src/showdown.js");
 // Some helpers
 var assert_equal = function(a, b) {
   if(a != b){
-    return { message: ("Expected:\n" + a + "\nGot:\n" + b), result: false};
+    throw { message: ("Expected:\n" + a + "\nGot:\n" + b), result: false};
   } else { 
     return {result: true};
   }
@@ -14,7 +14,13 @@ var indent = function(text) {
 }
 
 var it = function(name, code) {  
-  var res = code();
+  var res = {result: true}
+  try {
+    code();
+  } catch(er) {
+    res = er;
+  }
+
   if(res.result){
     console.log("\033[1;32mPassed: " + name + "\033[0m");
     return true;
@@ -34,26 +40,39 @@ var converter = new showdown.Showdown.converter();
 it("should make paragraphs out of simple texts", function() {
   var html = converter.makeHtml("Hallo");
   var expected = "<p>Hallo</p>"
-  return assert_equal(html, expected);
+  assert_equal(html, expected);
 });
 
 it("should parse Github-style code blocks", function() {
   var text = "```ruby\ndef hello_world\n  puts 'Hallo'\nend\n```";
   var html = converter.makeHtml(text);
   var expected = "<pre><code class='ruby'>def hello_world\n  puts 'Hallo'\nend\n</code></pre>";
-  return assert_equal(html, expected);
+  assert_equal(html, expected);
 });
 
 it("should make header tags", function() {
   var text = "# Header";
   var html = converter.makeHtml(text);
   var expected = "<h1 id=\"header\">Header</h1>";
-  return assert_equal(html, expected);
+  assert_equal(html, expected);
+
+  text = "## Header";
+  html = converter.makeHtml(text);
+  expected = "<h2 id=\"header\">Header</h2>";
+  assert_equal(html, expected);
 });
 
 it("should make header tags from underlined text", function() {
   var text = "Header\n======";
   var html = converter.makeHtml(text);
   var expected = "<h1 id=\"header\">Header</h1>";
-  return assert_equal(html, expected);
+  assert_equal(html, expected);
+});
+
+it("should make second level header tags from text underlined with underscores", function() {
+  var text = "Header\n------";
+  var html = converter.makeHtml(text);
+  var expected = "<h2 id=\"header\">Header</h2>";
+
+  assert_equal(html, expected);
 });
